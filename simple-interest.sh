8c94b7e46e419448e4bb93016a7b1a3ea37129b6
Marvin Doniña <44088592+jbmarvin24@users.noreply.github.com>
2023-04-28 10:28:01 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # jbmarvin24
    # Input:
    # p, principal amount
    # t, time period in years
